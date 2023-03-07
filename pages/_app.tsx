b592380c00a291c4ede621950fe978bd47b05a2b
tienchanhtri <tienchanhtri@gmail.com>
2023-03-07 14:43:28 +0700

    disable auto zoom in ios

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,16 @@ import '@/styles/globals.css'
 import type {AppProps} from 'next/app'
 import * as smoothscroll from 'smoothscroll-polyfill';
 import {useEffect} from "react";
+import Head from "next/head";
 
 export default function App({Component, pageProps}: AppProps) {
-  useEffect(() => {
-    smoothscroll.polyfill();
-  })
-  return <Component {...pageProps} />
+    useEffect(() => {
+        smoothscroll.polyfill();
+    })
+    return <>
+        <Head>
+            <meta name="viewport" content="initial-scale=1, maximum-scale=1"/>
+        </Head>
+        <Component {...pageProps} />
+    </>
 }
