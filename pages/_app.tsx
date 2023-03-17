a411ceab9503d803d3f13ffd1a184bec88cd80f4
tienchanhtri <tienchanhtri@gmail.com>
2023-03-17 07:33:04 +0700

    add firebase analytic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,14 @@ import type {AppProps} from 'next/app'
 import * as smoothscroll from 'smoothscroll-polyfill';
 import {useEffect} from "react";
 import Head from "next/head";
+import {getFirebaseAnalytic} from "@/lib/firebase";
 
 export default function App({Component, pageProps}: AppProps) {
     useEffect(() => {
         smoothscroll.polyfill();
+        getFirebaseAnalytic()
     }, [])
+
     return <>
         <Head>
             <meta name="viewport" content="initial-scale=1, maximum-scale=1"/>
